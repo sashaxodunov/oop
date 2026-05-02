8d30a695460b9d27e1d320f46f6b4f560d8e3b99
sashaxodunov <sasha@xodunov.ru>
2026-05-02 21:25:25 +0300

    Урок 15. Задание 2 и 3

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
-function DomElement(selector, height, width, bg, fontSize) {
-  this.selector = selector;
-  this.height = height;
-  this.width = width;
-  this.bg = bg;
-  this.fontSize = fontSize;
+// function DomElement(selector, height, width, bg, fontSize) {
+//   this.selector = selector;
+//   this.height = height;
+//   this.width = width;
+//   this.bg = bg;
+//   this.fontSize = fontSize;
 
-  this.createElement = function () {
+//   this.createElement = function () {
+//     let elem;
+
+//     if (this.selector.startsWith(".")) {
+//       elem = document.createElement("div");
+//       elem.classList.add(this.selector.slice(1));
+//     } else if (this.selector.startsWith("#")) {
+//       elem = document.createElement("p");
+//       elem.id = this.selector.slice(1);
+//     }
+
+//     elem.style.cssText = `
+//       height: ${this.height}px;
+//       width: ${this.width}px;
+//       background: ${this.bg};
+//       font-size: ${this.fontSize}px;
+//     `;
+
+//     elem.textContent = "Это созданный элемент";
+
+//     document.body.appendChild(elem);
+//   };
+// }
+
+class DomElement {
+  constructor(selector, height, width, bg, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+  }
+
+  createElement() {
     let elem;
 
     if (this.selector.startsWith(".")) {
@@ -23,8 +56,12 @@ function DomElement(selector, height, width, bg, fontSize) {
       font-size: ${this.fontSize}px;
     `;
 
-    elem.textContent = "Это созданный элемент";
-
+    elem.textContent = "Новый элемент";
     document.body.appendChild(elem);
-  };
+  }
 }
+
+const element = new DomElement(".block", 150, 300, "green", 18);
+
+// Вызов метода
+element.createElement();
